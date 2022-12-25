de60b086585167344f90781075c305c5b6adcd0e
amit jhariya <amit.jhariya93@gmail.com>
2022-12-25 13:11:37 +0530

    Start testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom'
 import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import Layout from './components/Layout';
+import { Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link 
+          to="/about">About
+        </Link>
+        <Link 
+          to="/">Home
+        </Link>
+        <Link 
+          to="/contact">Contact
+        </Link>
+       <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+      
     </div>
   );
 }
